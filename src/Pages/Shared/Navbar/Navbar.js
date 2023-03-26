7f41602b8b86c8e5b2423847d1a52a87ed7d1a4c
rajutalukdar1 <lalutaluker@gmail.com>
2023-03-26 22:01:26 +0600

    routes setup

---
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+
+    const menuItems = (
+        <>
+            <li className="font-semibold">
+                <Link to="/">Home</Link>
+            </li>
+            <li className="font-semibold">
+                <Link to="/blog">Blog</Link>
+            </li>
+        </>
+    );
+
+    return (
+        <div>
+            <div className="navbar stop bg-[#FAF7F5] bg-opacity-90 bg-transparent border-b-2">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                            {menuItems}
+                        </ul>
+                    </div>
+                    <Link to='/'><img className='w-20' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCUEwts4uoA2Zk4uGFSWayTNfoLgraSoxF2g&usqp=CAU' alt="" /></Link>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {menuItems}
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <div className="dropdown dropdown-end">
+                        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                            <div className="w-10 rounded-full">
+                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSZQEH2T7-ddsmNUm-Ff8qhVrhvv43XPQuCBzcmFrO&s" alt='' />
+                            </div>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                            <li>
+                                <Link className="justify-between">
+                                    Profile
+                                    <span className="badge">New</span>
+                                </Link>
+                            </li>
+                            <li><a>Settings</a></li>
+                            <li><a>Logout</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
