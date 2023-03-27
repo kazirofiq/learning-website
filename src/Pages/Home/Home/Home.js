aad68a0d9687dc17e4c18ca5a880fb3765ebd088
rajutalukdar1 <lalutaluker@gmail.com>
2023-03-27 16:30:35 +0600

    con

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import About from '../About/About';
+import Consultancy from '../Consultancy/Consultancy';
 import Course from '../Course/Course';
 
 const Home = () => {
     return (
         <div>
             <About></About>
-
             <Course></Course>
+            <Consultancy></Consultancy>
         </div>
     );
 };
