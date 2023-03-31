11df833a81524913df44d36feeebf3a65659bcd9
ahnaf4749 <ahnafsagor12@gmail.com>
2023-03-31 23:32:23 +0600

    succes modal added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -7,6 +7,7 @@ import WhatLearn from './WhatLearn/WhatLearn';
 import Banner from '../Banner/Banner';
 import Frequently from '../Frequently/Frequently';
 import StudentFeedback from '../StudentFeedback/StudentFeedback';
+import SuccesModal from '../../Modal/SuccesModal/SuccesModal';
 
 const Home = () => {
     return (
@@ -19,6 +20,7 @@ const Home = () => {
             <AmazingResource></AmazingResource>
             <StudentFeedback></StudentFeedback>
             <Frequently></Frequently>
+            {/* <SuccesModal></SuccesModal> */}
         </div>
     );
 };
