45c25a0074a6d2986e9716da62116e27cca11a02
Mahmudulmr19 <mahmudulmr19@gmail.com>
2023-04-07 22:54:52 +0600

    update class to className

--- src/Pages/Home/Banner/HowItWorksVideo.js
+++ src/Pages/Home/Banner/HowItWorksVideo.js
@@ -1,25 +1,33 @@
-import React from 'react';
+import React from "react";
 
 const HowItWorksVideo = () => {
-    return (
-        <>
-            <div>
-                <input
-                    type="checkbox"
-                    id="openVideoModal"
-                    className="modal-toggle" />
-                <div className="modal">
-                    <div className="modal-box relative ">
-                        <label
-                            htmlFor="openVideoModal"
-                            className="btn btn-sm btn-circle absolute right-2 top-2"
-                        >✕</label>
-                        <iframe className="w-full " width="363" height="363" src="https://www.youtube.com/embed/lJ5z7sdFMKo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div>
+        <input type="checkbox" id="openVideoModal" className="modal-toggle" />
+        <div className="modal">
+          <div className="modal-box relative ">
+            <label
+              htmlFor="openVideoModal"
+              className="btn btn-sm btn-circle absolute right-2 top-2"
+            >
+              ✕
+            </label>
+            <iframe
+              className="w-full "
+              width="363"
+              height="363"
+              src="https://www.youtube.com/embed/lJ5z7sdFMKo"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+            ></iframe>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default HowItWorksVideo;
